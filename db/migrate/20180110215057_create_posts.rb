295dc473231d032c4c352ffc8bc503ce6d96ecf2
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-10 19:52:31 -0200

    post: added scaffolding

--- db/migrate/20180110215057_create_posts.rb
+++ db/migrate/20180110215057_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.references :users, foreign_key: true
-      t.string :status
+      t.text :content
 
       t.timestamps
     end
