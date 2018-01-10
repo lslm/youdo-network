b0bee567fbc0f6aa1fdda5c452d901db141cc035
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-10 21:27:28 -0200

    users: método de login por nome de usuário
    Foram adicionados os campos "first_name", "last_name" e "username", além do método de login ter sido alterado de email para username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   has_many :posts
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+  
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
