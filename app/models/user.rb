b5d52d5c9b0eae82941be7dafc4484d2c5b285cc
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-10 22:25:39 -0200

    users: adicionado Friendly_id para roteamento de usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :username
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
