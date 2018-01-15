ccf0aae969bd1f43c5ff2814082bed29ca68adce
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-15 18:57:40 -0200

    fix: erro do logout em produção está relacionado à ordem de inclusão do Jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require semantic-ui
 //= require rails-ujs
 //= require turbolinks
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
 
