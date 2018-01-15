78e124a0f413a2f8f1ba4748ddbc13b4df1b2f40
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-15 01:05:02 -0200

    feed: milhorias no layout do feed de publicações

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,5 +1,5 @@
 class FeedController < ApplicationController
   def index
-    @posts = Post.where(user_id: Relationship.select(:following_id).where(follower_id: current_user.id))
+    @posts = Post.where(user_id: Relationship.select(:following_id).where(follower_id: current_user.id)).includes(:user)
   end
 end
