51cd572c2c78b0581836f7f9bfc416c7c80a93ce
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-16 01:21:39 -0200

    redef: refinamentos
    Adicionado possibilidade do usuário logado alterar seus dados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :avatar]
+    added_attrs = [:username, :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :avatar, :description]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
