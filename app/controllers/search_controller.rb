52ba835b56968206b4c4bae38305f0ff0057d6e9
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-16 00:45:24 -0200

    redef: refinamento na tela de pesquisa

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,6 @@ class SearchController < ApplicationController
   def index
     @users = User.search(params[:term])
     @posts = Post.search(params[:term])
+    @users_directory = User.all
   end
 end
