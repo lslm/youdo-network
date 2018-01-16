742a9d8dcfa082a331cda6cd796f3dd98361712b
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-01-16 12:56:23 -0200

    fix: publicações do usuário logado aparecerão no feed de publicações

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,5 +1,17 @@
 class FeedController < ApplicationController
   def index
-    @posts = Post.where(user_id: Relationship.select(:following_id).where(follower_id: current_user.id)).includes(:user)
+    @posts = Array.new
+
+    current_user.posts.each do |current_user_posts|
+      @posts << current_user_posts
+    end
+
+    current_user.following.each do |user|
+      user.posts.each do |post|
+        @posts << post
+      end
+    end
+
+    @posts.sort_by! { |post| post[:created_at] }
   end
 end
